d710506d09d62ccdaadf9c97f8b614541fbc83ea
Omar Abud <oaamendoza1972@gmail.com>
2025-05-27 00:49:13 -0300

    cambio dos

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # africa
-pagina dedicada a Africa
+pagina dedicada a Africa 
---
+++ script.js
@@ -0,0 +1,17 @@
+ document.addEventListener('DOMContentLoaded', () => {
+            // Lógica para el lightbox (visor de imágenes)
+            const sliderImages = document.querySelectorAll('.slider img');
+            const lightboxOverlay = document.getElementById('lightboxOverlay');
+            const lightboxImage = document.getElementById('lightboxImage');
+
+            sliderImages.forEach(image => {
+                image.addEventListener('click', () => {
+                    lightboxImage.src = image.src;
+                    lightboxOverlay.classList.add('active');
+                });
+            });
+
+            lightboxOverlay.addEventListener('click', () => {
+                lightboxOverlay.classList.remove('active');
+            });
+        });
